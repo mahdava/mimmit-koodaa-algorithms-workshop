dc2db978533060b4e7667b00478238a186cc50ef
mahdava <lucia.nazzaro@futurice.com>
2019-11-22 12:36:14 +0200

    Something

---
+++ section-1/section-1.js
@@ -0,0 +1,48 @@
+// Exercise 1: Uncomment the following lines of code cmd + k + u
+
+// document.write(
+//   "Yay! If you can see this in the preview, please comment me again! cmd + k + c."
+// );
+
+//////////////////////////////////////////////////////////////////////////
+
+// Note: for all these exercises, please use console.log() to display the results. Alternatively, feel free to use document.write()
+
+//////////////////////////////////////////////////////////////////////////
+
+// Exercise 2: Write a console.log() statement
+
+//////////////////////////////////////////////////////////////////////////
+
+// Exercise 3a: Write variables using either let or const for the following data types:
+
+// Exercise 3b: Write a string variable using const and display its value with console.log()
+
+//////////////////////////////////////////////////////////////////////////
+
+// Exercise 4a: Sum two number variables, display the result with console.log()
+
+// Exercise 4b: Concatenate two strings variables together, display the result with console.log()
+
+//////////////////////////////////////////////////////////////////////////
+
+// Exercise 5a: Write an array of people's first name and an array (with the same number of items) of numbers for their age
+
+// Exercise 5b: Read the number of the people's array with the property .length
+
+// Exercise 5c: Add a person to the people array with the method .push(), and do the same for their age
+
+// Exercise 5d: Write a custom more verbose version of the method .push()
+
+// Exercise 5e: Use the pop() method to remove and read the last element for both the people and age arrays
+
+//////////////////////////////////////////////////////////////////////////
+
+// Exercise 6a: Create a person object with firstName, lastName and age (remember that age is a number and not a string)
+
+// Example of Car Object:
+// const car = {"type": "Fiat", model: "500", "color": "white"};
+
+// Exercise 6b: The person you just created has had a sudden birthday, add +1 to the age using operations
+
+// Exercise 6c: Create an array of person objects like the one we created before (All people need to have firstName, lastName and age)
---
+++ section-2/INSTRUCTIONS.md
@@ -0,0 +1,10 @@
+What is a Data Structure?
+A data structure is a particular way of organizing data in a computer so that it can be used effectively.
+(from: GeeksForGeeks)
+
+What about algorithms?
+An algorithm is a step by step method of solving a problem.
+It is commonly used for data processing, calculation and other related computer and mathematical operations.
+An algorithm is also used to manipulate data in various ways, such as inserting a new data item,
+searching for a particular item or sorting an item.
+(from: Techopedia)
